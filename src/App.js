1462e70d4cb91416320cc7052e9a6cf3bfa6e098
jungyungee <86039030+jungyungee@users.noreply.github.com>
2023-06-17 22:52:51 +0900

    스타일드 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Movie from './components/Movie';
 import { dummy } from './movieDummy';
 
 function App() {
   return (
-    <div>
-      <h1>MOVIES NOW</h1>
+    <AppWrap>
+      <h1>MOVIES NOW?!</h1>
       <div className='app-container'>{
         dummy.results.map((item)=>{
           return(
@@ -18,8 +20,34 @@ function App() {
         })
       }     
       </div>
-    </div>
+    </AppWrap>
   );
 }
 
 export default App;
+
+const AppWrap = styled.div`
+  color: black;
+  text-align: center;
+  h1{
+
+  }
+  .app-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    background-color: white;
+  }
+  .movie-container {
+    width: 250px;
+    border: 1px solid white;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px 5px gray;
+    margin: 10px;
+  }
+  .movie-container img{
+    max-width: 100%;
+    border-radius: 10px;
+  }
+`
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
 
@@ -8,7 +9,7 @@ export default function Movie({title, poster_path, vote_average}){
             <img src={IMG_BASE_URL + poster_path} alt="영화포스터" />
             <div className="movie-info">
                 <h4>{title}</h4>
-                <span>{vote_average}</span>
+                <span>⭐️{vote_average}</span>
             </div>
         </div>
     )
